178a68c99f4b9e552cff0c2b8e3d58a08e2020ca
ameyaaklekar <ameyaaklekar@gmail.com>
2020-07-25 00:47:14 +1200

    Fixed the route for roles permission

--- src/store/employee.js
+++ src/store/employee.js
@@ -29,7 +29,7 @@ export default {
 
     async getRolePermission(_, data) {
       try {
-        let response = await axios.get("api/rolesPermission", {
+        let response = await axios.get("api/roles/permission", {
           params: {
             role: data
           }
